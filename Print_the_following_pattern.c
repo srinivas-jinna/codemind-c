31e0d93695e90d13764b54fe1792d172a12e14ce
srinivas-jinna <119839020+srinivas-jinna@users.noreply.github.com>
2023-01-25 20:53:04 +0530

    #include<stdio.h> int main() {     int i,j,n,p;     scanf("%d",&n);     p=n+1;     for(i=1;i<=n;i++)     {         p--;         for(j=1;j<=n;j++)         {             if(i+j==n+1 || i==j)             {                 printf("%d ",p);             }             else             {             	printf(" "); 			}         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,p;
+    scanf("%d",&n);
+    p=n+1;
+    for(i=1;i<=n;i++)
+    {
+        p--;
+        for(j=1;j<=n;j++)
+        {
+            if(i+j==n+1 || i==j)
+            {
+                printf("%d ",p);
+            }
+            else
+            {
+            	printf(" ");
+			}
+        }
+        printf("
+");
+    }
+}
